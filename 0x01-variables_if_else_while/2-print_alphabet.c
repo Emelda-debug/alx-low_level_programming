60f0a5294e4f4229411cd1c34087bb3b8a53feb3
Emelda-debug <emeldamada5@gmail.com>
2023-09-08 15:55:19 -0700

    task 2-5 corrections

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,15 @@
 
 
 /**
- *  main - program that prints the alphabet in lowercase and upper case
+ * main - program that prints the alphabet in lowercase
  *
  *  Return: Always 0.
  */
-
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	putchar(letter);
-
-	for (letter = 'A'; letter <= 'Z'; letter++)
 	putchar(letter);
 	putchar('\n');
 	return (0);
