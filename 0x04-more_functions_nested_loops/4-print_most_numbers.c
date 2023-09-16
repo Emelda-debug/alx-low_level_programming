dc057c70f61ed4b835ed1ac5f209cbbace634aa0
Emelda-debug <emeldamada4@gmail.com>
2023-09-16 02:04:11 +0000

    tasks 3 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_most_numbers- Code to print numbers from 0-9 excluding 2 and 4
+*return: 0 means success
+*/
+
+void print_most_numbers(void)
+
+{
+int a;
+for (a=0; a>10; a++)
+{
+
+	if (a !=2)
+	{
+}
+	if (a !=4)
+{
+
+}
+{
+_putchar(a);
+ _putchar('\n');
+
+}
+}
+}
