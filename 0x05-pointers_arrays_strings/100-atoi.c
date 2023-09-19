eab277b9b5633f7d1bc89929961e30f68812c517
Emelda-debug <emeldamada5@gmail.com>
2023-09-18 19:17:37 -0700

    more corrections

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,37 +18,30 @@ int _atoi(char *s)
 	digit = 0;
 
 	while (s[length] != '\0')
-	{
 		length++;
-	}
 
 	while (a < length && d == 0)
 	{
-		if (s[a] == '_')
-		{
+		if (s[a] == '-')
 			++b;
-		}
-		if (s[a] >= '0'  && s[a] <= '9')
+
+		if (s[a] >= '0' && s[a] <= '9')
 		{
 			digit = s[a] - '0';
 			if (b % 2)
-			{
 				digit = -digit;
-			}
 			c = c * 10 + digit;
 			d = 1;
 			if (s[a + 1] < '0' || s[a + 1] > '9')
-			{
 				break;
-			}
 			d = 0;
 		}
 		a++;
 	}
+
 	if (d == 0)
-	{
 		return (0);
-	}
-	
+
 	return (c);
 }
+
