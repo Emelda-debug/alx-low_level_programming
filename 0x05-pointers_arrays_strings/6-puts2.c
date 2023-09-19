335bbbb7fa860c10f41c4e5fe4ec5f7c008195dc
Emelda-debug <emeldamada5@gmail.com>
2023-09-18 17:35:11 -0700

    more corrections

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,28 +10,25 @@
 
 void puts2(char *str)
 {
+	int y = 0;
+	int z = 0;
+	char *b = str;
+	int c;
 
-        int y = 0;
-        int z = 0;
-        char *b = str;
-        int c;
+	while (*b != '\0')
+	{
+		b++;
+		y++;
+	}
 
-        while (*b != '\0')
-        {
-                b++;
-                y++;
-        }
-
-        z = y-1;
-        for (c=0; c <= z; c++ )
-        {
-                if (c % 2 ==0)
-                {
-                        _putchar(str[c]);
-                }
-        }
-
-        _putchar('\n');
+	z = y - 1;
+	for (c = 0; c <= z; c++)
+	{
+		if (c % 2 == 0)
+		{
+			_putchar(str[c]);
+		}
+	}
+	_putchar('\n');
 }
-
 
