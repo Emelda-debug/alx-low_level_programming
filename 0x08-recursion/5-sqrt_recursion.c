dc96705b3c1dab9cf984465cf68873276877e211
Emelda-debug <emeldamada4@gmail.com>
2023-09-28 16:56:45 +0000

    task 0 and 5 corrections

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,13 @@
 #include "main.h"
-int _example_sqrt_recursion(int a, int b)
+
+int _example_sqrt_recursion(int n, int b);
+
 /**
  * _sqrt_recursion-  function that returns the
- *  natural square root of a number
- *  @n: the variable for the number whose
- *  square root will be determined
- *  Return: always 0 success
+ * natural square root of a number
+ * @n: the variable for the number whose
+ * square root will be determined
+ * Return: always 0 success
  */
 
 int _sqrt_recursion(int n)
@@ -16,27 +18,27 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		return (example_sqrt_recursion(n, 0));
+		return (_example_sqrt_recursion(n, 0));
 	}
-
 }
+
 /**
- * _example_sqrt_recursion- Function to demonstrate square root
- * of a natural number recursively
- * @a: number whose square root will be calculated
- * @b: the iterator
- * Return: the square root of a
+ *  _example_sqrt_recursion- Function to demonstrate square root
+ *  of a natural number recursively
+ *  @n: number whose square root will be calculated
+ *  @b: the iterator
+ *  Return: the square root of a
  */
-int _example_sqrt_recursion(int a, int b)
+
+int _example_sqrt_recursion(int n, int b)
 {
-	if (b * b > a)
+	if (b * b > n)
 	{
 		return (-1);
 	}
-	if (b * b == a)
+	if (b * b == n)
 	{
-		return (a);
+		return (b);
 	}
-	return (example_sqrt_recursion(a, b + 1));
-
+	return (_example_sqrt_recursion(n, b + 1));
 }
