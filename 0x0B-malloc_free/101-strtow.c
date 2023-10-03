78e7d7b19ce6296f66a7e73755ebb43b2d6b777e
Emelda-debug <emeldamada4@gmail.com>
2023-10-03 20:27:11 +0000

    more corrections

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * strtow- function that splits a string into words
+ * @str: input string
+ * Return: NULL if str == NULL or str == ""
+ */
+
+char **strtow(char *str)
+{
+}
