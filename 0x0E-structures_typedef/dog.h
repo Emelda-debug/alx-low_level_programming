d56e0dc06de508354bf2f5c092e38139af843c08
Emelda-debug <emeldamada4@gmail.com>
2023-10-10 10:39:24 +0000

    task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_FILE
+#define DOG_FILE
+/**
+ * struct dog- new type
+ * @name: dog name of char data type
+ * @float: age of dog in decimal points
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
